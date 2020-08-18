a7386b837f83ae8118bb420aebe38ad8834597c2
shuhao.yan <shuhao.yan@united-imaging.com>
2020-08-18 10:42:13 +0800

    refactor: 个人中心重构

--- miniprogram/common/tool.js
+++ miniprogram/common/tool.js
@@ -38,3 +38,16 @@ export const uuid =function () {
   var uuid = s.join("");
   return uuid;
 }
+
+// 空对象
+export function isEmptyObject (obj) {
+  if (!obj) {
+      return true
+  }
+  for (const prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+          return false
+      }
+  }
+  return true
+}
--- miniprogram/pages/personal/personal.js
+++ miniprogram/pages/personal/personal.js
@@ -1,41 +1,63 @@
 // miniprogram/pages/personal/personal.js
-const app= getApp()
+const app = getApp()
+const tool = require('../../common/tool.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    avatarUrl: '/images/personal.svg',
+    avatarUrl: '',
     userName: '登录',
     userInfo: {},
     logged: false,
     takeSession: false,
-    requestResult: ''
+    requestResult: '',
+    loading: true
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo,
-                userName: res.userInfo.nickName,
-                logged: true
-              })
-              this.onGetOpenid()
-            }
-          })
+    let self = this
+    let userInfo = wx.getStorageSync("userInfo")
+    if (tool.isEmptyObject(userInfo)) {
+      wx.getSetting({
+        success(res) {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                self.setData({
+                  avatarUrl: res.userInfo.avatarUrl,
+                  userInfo: res.userInfo,
+                  userName: res.userInfo.nickName,
+                  loading: false,
+                  logged: true
+                })
+                // 缓存用户信息
+                wx.setStorageSync("userInfo", res.userInfo)
+                self.onGetOpenid()
+              }
+            })
+          } else {
+            self.setData({
+              avatarUrl: '/images/default-image.png',
+              loading: false,
+              logged: false
+            })
+          }
         }
-      }
-    })
-  
+      })
+    } else {
+      self.setData({
+        avatarUrl: userInfo.avatarUrl,
+        userInfo: userInfo,
+        userName: userInfo.nickName,
+        loading: false,
+        logged: true
+      })
+    }
   },
   onGetUserInfo: function (e) {
     if (!this.data.logged && e.detail.userInfo) {
@@ -45,8 +67,9 @@ Page({
         userInfo: e.detail.userInfo,
         userName: e.detail.userInfo.nickName
       })
-     
     }
+    // 缓存用户信息
+    wx.setStorageSync("userInfo", e.detail.userInfo)
     this.onGetOpenid()
   },
   onGetOpenid: function () {
@@ -61,15 +84,13 @@ Page({
           data: res.result.openid,
         })
         app.globalData.openid = res.result.openid
-       
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
-      
       }
     })
   },
-  createHistory(){
+  createHistory() {
     if (this.data.logged) {
       wx.navigateTo({
         url: '/pages/suggestion/suggestion',
@@ -81,18 +102,40 @@ Page({
       })
     }
   },
-  getHistory(){
-    if(this.data.logged){
-     wx.navigateTo({
-       url: '/pages/historys/historys',
-     })
-   }else{
-     wx.showToast({
-       icon: "none",
-       title: '请先登录小程序',
-     })
-   }
-   
+  getHistory() {
+    if (this.data.logged) {
+      wx.navigateTo({
+        url: '/pages/historys/historys',
+      })
+    } else {
+      wx.showToast({
+        icon: "none",
+        title: '请先登录小程序',
+      })
+    }
+
+  },
+  /**
+  * 退出登录
+  */
+  loginOut() {
+    let self = this
+    wx.showModal({
+      title: '提示',
+      content: '您确定要退出登录吗',
+      success: function (res) {
+        if (res.confirm) {
+          // 缓存用户信息清空
+          self.setData({
+            avatarUrl: '/images/default-image.png',
+            userInfo: {},
+            userName: '登录',
+            logged: false
+          })
+          wx.setStorageSync('userInfo', {})
+        }
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
